62d55818784bb2eddbac9a66622117a849fcb76b
Chris Vasquez <chrvasq@outlook.com>
2019-06-03 19:29:26 -0700

    Combine clean_data function with extract_data function

--- app.py
+++ app.py
@@ -8,17 +8,13 @@ from datetime import date
 db = SqliteDatabase('inventory.db')
 
 
-def extract_data(csv_file):
-    # extract data
+def extract_and_clean_data(csv_file):
+    # read and extract data
     with open(csv_file, newline='') as csvfile:
         inventory = csv.reader(csvfile, delimiter=',')
         rows = list(inventory)
         header = rows[0]
         product_data = rows[1:]
-    return header, product_data
-
-def clean_data(csv_file):
-    header, product_data = extract_data(csv_file)
     # clean data
     for product in product_data:
         product[1] = int(product[1].lstrip('$').replace('.', ''))
@@ -29,16 +25,13 @@ def clean_data(csv_file):
                           int(last_updated[1]))
     return header, product_data
 
-def transform_data(input_csv_file, output_csv_file='clean_inventory.csv'):
-    header, product_data = clean_data(input_csv_file)
-    # transform data
-    with open(output_csv_file, 'w') as csvfile:
-        inventory_writer = csv.DictWriter(csvfile, fieldnames=header)
-        inventory_writer.writeheader()
-        for product in product_data:
-            inventory_writer.writerow({header[0]: product[0],
-                                       header[1]: product[1],
-                                       header[2]: product[2],
-                                       header[3]: product[3]})
-
-transform_data('inventory.csv')
+def transform_data(csv_file):
+    header, product_data = extract_and_clean_data(csv_file)
+    # transform data to list of dictionary entries for each product
+    inventory = []
+    for product in product_data:
+        inventory.append({header[0]: product[0],
+                          header[1]: product[1],
+                          header[2]: product[2],
+                          header[3]: product[3]})
+    return inventory
