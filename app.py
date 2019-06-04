a3fd6eb14c76bff1504dcd50566121f25323ffc9
Chris Vasquez <chrvasq@outlook.com>
2019-06-03 20:57:04 -0700

    Moved functions and model to separate modules in storeinventory package

--- app.py
+++ app.py
@@ -1,58 +1,7 @@
 #!/usr/bin/env python3
-import csv
-from datetime import date
+from storeinventory.etl import (Product, db, extract_and_clean_data,
+                                transform_data, add_data_from_csv)
 
-from peewee import *
-
-db = SqliteDatabase('inventory.db')
-
-
-class Product(Model):
-    product_id = AutoField(primary_key=True)
-    product_name = TextField()
-    product_quantity = IntegerField()
-    product_price = IntegerField()
-    date_updated = DateField()
-
-    class Meta:
-        database = db
-
-
-def extract_and_clean_data(csv_file):
-    # read and extract data
-    with open(csv_file, newline='') as csvfile:
-        inventory = csv.reader(csvfile, delimiter=',')
-        rows = list(inventory)
-        header = rows[0]
-        product_data = rows[1:]
-    # clean data
-    for product in product_data:
-        product[1] = int(product[1].lstrip('$').replace('.', ''))
-        product[2] = int(product[2])
-        last_updated = product[3].split('/')
-        product[3] = date(int(last_updated[2]),
-                          int(last_updated[0]),
-                          int(last_updated[1]))
-    return header, product_data
-
-
-def transform_data(csv_file):
-    header, product_data = extract_and_clean_data(csv_file)
-    # transform data to list of dictionary entries for each product
-    inventory = []
-    for product in product_data:
-        inventory.append({header[0]: product[0],
-                          header[1]: product[1],
-                          header[2]: product[2],
-                          header[3]: product[3]})
-    return inventory
-
-def add_data_from_csv(product_list):
-    for product in product_list:
-        Product.create(product_name=product['product_name'],
-                       product_quantity=product['product_quantity'],
-                       product_price=product['product_price'],
-                       date_updated=product['date_updated'])
 
 if __name__ == "__main__":
     db.connect()
