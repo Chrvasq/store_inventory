0dd8dabc15963b80135cae95b8a6cfc0444f5514
Chris Vasquez <chrvasq@outlook.com>
2019-06-05 22:43:04 -0700

    Implement get_view_product_input method

--- storeinventory/menu.py
+++ storeinventory/menu.py
@@ -1,5 +1,6 @@
 from .database import Database
 from datetime import date
+from .product import Product
 
 
 class Menu:
@@ -8,7 +9,7 @@ class Menu:
                              'a': 'Add a product to the inventory database',
                              'b': 'Backup database to a .CSV file'}
 
-    def display_main_menu(self):
+    def display_menu(self):
         for key, value in self.menu_options.items():
             print(f'{key}) {value}')
 
@@ -22,6 +23,22 @@ class Menu:
         except ValueError:
             print('Invalid choice. Please enter v/a/b.')
             self.get_menu_input()
+    
+    def get_view_product_input(self):
+        try:
+            user_input = input('Please enter in a product ID: ')
+            if user_input.isalpha():
+                raise ValueError(
+                    'You didn\'t enter a number. Please enter a number.')
+            # Check for product id membership
+            if int(user_input) not in [
+                product.product_id for product in Product.select()]:
+                    raise ValueError('Product ID not found.')
+            else:
+                Database.view_product(user_input)
+        except ValueError as error:
+            print(error)
+            self.get_view_product_input()
 
     def main(self):
         pass
